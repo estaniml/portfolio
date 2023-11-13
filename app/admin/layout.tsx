4cbd4216883ac0aee3bd18b019e7f5e76bf185d3
Estanislao Martinez Lacabe <emartinezlacabe@gmail.com>
2023-11-13 12:42:20 -0300

    adding vercel analytics

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from 'next'
+import { Analytics } from '@vercel/analytics/react';
 
 export const metadata: Metadata = {
   title: "Web Developer - Estanislao Martinez Lacabe",
@@ -12,7 +13,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        {children}
+        <Analytics />
+      </body>
     </html>
   )
 }
